4a7169ee95a64a71e78ba0ad14e30a7da880fda4
Marco Knuettel <marco@knuettel.de>
2012-08-25 11:41:35 +0200

    migrate image-urls

--- blog/conv.sh
+++ blog/conv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for file in $(ls *.erb) ; do
    echo ${file}
-   sed -e "s/\[map/<div class=\"map\"/g" ${file} | sed -e "s/ style=\"width:763px; height:350px; border:1px solid gray; margin-right:0px; margin-bottom:15px; float:left;\"\]&nbsp;/><\/div>/g" > "${file}.txt"
+   # sed -e "s/\[map/<div class=\"map\"/g" ${file} 
+   sed -e "s/<img src='\/posts/<img src='\.\.\/\\.\.\/\.\.\/\\.\.\/posts/g" ${file} > "${file}.txt"
    mv "${file}.txt" "${file}"
 done
