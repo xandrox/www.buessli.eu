c4891a9fde89e6038c2c8f1a387e0d8aa5ab14e6
Marco Knuettel <marco@knuettel.de>
2012-08-25 11:57:30 +0200

    migrate image/gpx-urls

--- blog/conv.sh
+++ blog/conv.sh
@@ -2,6 +2,6 @@
 for file in $(ls *.erb) ; do
    echo ${file}
    # sed -e "s/\[map/<div class=\"map\"/g" ${file} 
-   sed -e "s/<img src='\/posts/<img src='\.\.\/\\.\.\/\.\.\/\\.\.\/posts/g" ${file} > "${file}.txt"
+   sed -e "s/\/gpx\//<%= @site.base_url %>\/gpx\//g" ${file} > "${file}.txt"
    mv "${file}.txt" "${file}"
 done
