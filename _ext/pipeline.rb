d8183903b391c19ac42deb011b35fcddb6d64c51
Marco Knuettel <marco@knuettel.de>
2012-08-26 02:39:24 +0200

    header logo

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,8 @@
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/blog', :posts )
+#  extension Awestruct::Extensions::Tagger.new( :posts, '/blog/index', '/blog/tags', :per_page=>10, :sanitize=>true )
+#  extension Awestruct::Extensions::TagCloud.new(:posts, '/blog/tags/index.html', :layout=>'tab', :title=>'Tags')
   # extension Awestruct::Extensions::Paginator.new( :posts, '/blog/index', :per_page=>10 )
   # extension Awestruct::Extensions::Indexifier.new
 end
