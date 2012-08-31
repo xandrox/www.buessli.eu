61464d84e31f9639e52f057a2bee83b6dd3e1d2a
Marco Knuettel <marco@knuettel.de>
2012-08-31 15:20:42 +0200

    atom feed

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -6,5 +6,11 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/europareise2012', :europareise2012s )
   extension Awestruct::Extensions::Posts.new( '/busausbau', :busausbaus )
   extension Awestruct::Extensions::Buessli.new
+  extension Awestruct::Extensions::Atomizer.new( 
+    :europareise2012s, 
+    '/europareise2012/feed.atom', 
+    :num_entries=>10000,
+    :content_url=>'https://www.buessli.eu/europareise2012',
+    :feed_title=> 'No Relation To' )
 end
 
